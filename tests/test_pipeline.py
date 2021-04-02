42097822472c52d72de47d9b2d5114e74b5d9fb2
Carina Lansing <carina.lansing@pnnl.gov>
2021-04-02 11:20:08 -0700

    Added a unit test.

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,12 +1,21 @@
+import os
+import sys
 import unittest
+
+# Add the parent directory to the pythonpath
+test_dir_path = os.path.dirname(os.path.realpath(__file__))
+project_path = os.path.dirname(test_dir_path)
+sys.path.append(project_path)
+
+from pipeline.runner import run_pipeline
 
 
 class TestPipeline(unittest.TestCase):
 
-
     def test_pipeline(self):
         # Run the pipeline via the runner
-        pass
+        run_pipeline(os.path.join(test_dir_path, "data"))
+
 
 if __name__ == '__main__':
     unittest.main()
